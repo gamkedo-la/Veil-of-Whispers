ba7c8af7403a298ae4c7d003ba3c6aa22766759e
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-10-24 19:29:15 +0500

    Added State,StateMachine, PlayerStateMachine and PlayerBaseState Classes

---
+++ Assets/Scripts/StateMachines/PlayerStates/PlayerBaseState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerBaseState : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
