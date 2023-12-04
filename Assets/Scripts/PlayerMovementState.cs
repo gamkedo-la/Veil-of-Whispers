816de96764fa2f245f2576441f6cbb814a2c7976
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-12-05 00:23:27 +0500

    Added Player Idle and Running Animation

--- Assets/Scripts/PlayerMovementState.cs
+++ Assets/Scripts/PlayerMovementState.cs
@@ -19,6 +19,8 @@ public class PlayerMovementState : PlayerBaseState
     public override void Tick(float deltaTime)
     {
         Vector3 movement = new Vector3();
+
+
         movement.x = stateMachine.InputReader.MovementValue.x;
         movement.y = 0;
         movement.z = stateMachine.InputReader.MovementValue.y;
@@ -31,6 +33,8 @@ public class PlayerMovementState : PlayerBaseState
 
         if (stateMachine.InputReader.MovementValue == Vector2.zero) { return; }
 
+        stateMachine.transform.rotation = Quaternion.LookRotation(movement);
+        
     }
 
     public override void Exit()
