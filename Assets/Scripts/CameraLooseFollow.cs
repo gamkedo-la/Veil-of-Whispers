8281c07989ee44f0589f55ebe2d5cf8cb0018fe1
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-12-21 05:30:26 +0500

    Improve Camera

--- Assets/Scripts/CameraLooseFollow.cs
+++ Assets/Scripts/CameraLooseFollow.cs
@@ -1,42 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraLooseFollow : MonoBehaviour
 {
-    public Transform focusLocation;
+    public Transform playerTransform;
+    public float rotationSpeed = 2.0f;
+    public float distance = 5.0f;
+    public float heightOffset = 2.0f;
+    public float maxUpwardRotation = 80.0f; // Maximum upward rotation angle
+    public float maxDownwardRotation = 80.0f; // Maximum downward rotation angle
     private Camera chaseCamera;
-    private float distanceSinceLastCameraMove = 0.0f;
-    private float movedBodyEnoughToMoveCameraThreshold = 15.0f;
-    private Vector3 previousFocusLocation;
 
-    // Start is called before the first frame update
-    void Start()
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
+    private void Start()
     {
         chaseCamera = Camera.main;
-        previousFocusLocation = transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
-        distanceSinceLastCameraMove = Vector3.Distance(previousFocusLocation,
-                            focusLocation.position);
-        if(distanceSinceLastCameraMove > movedBodyEnoughToMoveCameraThreshold)
-        {
-            previousFocusLocation = focusLocation.position;
-        }
-        Debug.Log(distanceSinceLastCameraMove);
+        float mouseX = Mouse.current.delta.x.ReadValue();
+        float mouseY = Mouse.current.delta.y.ReadValue();
+
+        // Adjust the vertical rotation
+        rotationX -= mouseY * rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX, -maxDownwardRotation, maxUpwardRotation);
+
+        // Adjust the horizontal rotation
+        rotationY += mouseX * rotationSpeed;
     }
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
-        float fitTuningNumber = 0.04f;
-        chaseCamera.transform.position = Vector3.Lerp(chaseCamera.transform.position, transform.position, fitTuningNumber);
-    }
+        // Convert vertical rotation to Quaternion
+        Quaternion verticalRotation = Quaternion.Euler(rotationX, 0, 0);
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        chaseCamera.transform.LookAt(focusLocation);
+        // Convert horizontal rotation to Quaternion
+        Quaternion horizontalRotation = Quaternion.Euler(0, rotationY, 0);
+
+        // Combine both rotations
+        Quaternion combinedRotation = verticalRotation * horizontalRotation;
+
+        // Set the camera's position to the player's position with the offset
+        Vector3 offset = combinedRotation * new Vector3(0, heightOffset, -distance);
+        chaseCamera.transform.position = playerTransform.position + offset;
+
+        // Make the camera look at the player's position
+        chaseCamera.transform.LookAt(playerTransform.position + Vector3.up * heightOffset);
     }
 }
