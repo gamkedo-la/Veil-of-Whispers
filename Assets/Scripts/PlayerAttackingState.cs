2bcaed2413bc69e7d69d41e7f983c11fb56c8945
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-12-22 02:06:34 +0500

    Added Combo Attacks

--- Assets/Scripts/PlayerAttackingState.cs
+++ Assets/Scripts/PlayerAttackingState.cs
@@ -1,26 +1,87 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerAttackingState : PlayerBaseState
 {
     private Attack attack;
-    public PlayerAttackingState(PlayerStateMachine stateMachine, int attackId) : base(stateMachine)
+
+    private float previousFrameTime;
+    public PlayerAttackingState(PlayerStateMachine stateMachine, int attackIndex) : base(stateMachine)
     {
-        attack = stateMachine.Attacks[attackId];
+        attack = stateMachine.Attacks[attackIndex];
     }
 
     public override void Enter()
     {
-        stateMachine.animator.CrossFadeInFixedTime(attack.AnimationName, 0.1f);
+        stateMachine.animator.CrossFadeInFixedTime(attack.AnimationName, attack.TransitionDuration);
     }
 
     public override void Tick(float deltaTime)
     {
+        
+
+        float normalizedTime = GetNormalizedTime();
+
+        if(normalizedTime > previousFrameTime && normalizedTime < 1f) {
+
+            if(stateMachine.InputReader.isAttacking)
+            {
+                TryComboAttack(normalizedTime);
+            }
+        }
+
+        previousFrameTime = normalizedTime;
     }
+
+    private void TryComboAttack(float normalizedTime)
+    {
+        if ((attack.ComboStateIndex == -1)){ return; }
+
+        if (normalizedTime < attack.ComboAttackTime) { return; }
+
+        stateMachine.SwitchState
+        (
+
+        new PlayerAttackingState
+        (
+            stateMachine,
+            attack.ComboStateIndex
+        )
+        
+        );
+    }
+      
 
     public override void Exit()
     {
+
+    }
+
+   
+
+    private float GetNormalizedTime()
+    {
+       AnimatorStateInfo currentInfo = stateMachine.animator.GetCurrentAnimatorStateInfo(0);
+       AnimatorStateInfo nextInfo    = stateMachine.animator.GetCurrentAnimatorStateInfo(0);
+
+        if(stateMachine.animator.IsInTransition(0) && nextInfo.IsTag("Attack"))
+        {
+            return nextInfo.normalizedTime;
+        }
+
+        else if(stateMachine.animator.IsInTransition(0) && nextInfo.IsTag("Attack"))
+        {
+            return currentInfo.normalizedTime;
+        }
+
+        else
+        {
+            return 0;
+        }
+
     }
 
 }
