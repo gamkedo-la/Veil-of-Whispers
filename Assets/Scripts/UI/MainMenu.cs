43b7f70802f738a504c908f3ce1b3342c0a4ab40
Tyler Funk <tcfunk25@gmail.com>
2023-12-22 15:34:51 -0600

    add TMP; created main menu scene and buttons

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private Button PlayButton;
+        [SerializeField]
+        private Button QuitButton;
+        [SerializeField]
+        private Button SettingsButton;
+
+        private void Start()
+        {
+            PlayButton.onClick.AddListener(() =>
+            {
+                Debug.Log("Play Button");
+            });
+            SettingsButton.onClick.AddListener(() =>
+            {
+                Debug.Log("Settings button");
+            });
+            QuitButton.onClick.AddListener(() =>
+            {
+                Debug.Log("Quit Button");
+            });
+        }
+    }
+}
