2676aa4691e974bbe2ecb8abd2918f41f7aa9fa5
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2024-01-05 02:32:50 +0500

    Added Player Hanging State

---
+++ Assets/Scripts/StateMachines/PlayerStates/PlayerHangingState.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHangingState : PlayerBaseState
+{
+    private readonly int HangHash = Animator.StringToHash("Hanging");
+    private const float CrossFadeDuration = 0.1f;
+    private const float AnimatorDampTime = 0.1f;
+    private Vector3 closestPoint;
+    private Vector3 ledgeForward;
+    public PlayerHangingState(PlayerStateMachine stateMachine, Vector3 closestPoint, Vector3 ledgeForward) : base(stateMachine)
+    {
+        this.closestPoint = closestPoint;
+        this.ledgeForward = ledgeForward;
+    }
+
+    public override void Enter()
+    {
+        stateMachine.transform.rotation = Quaternion.LookRotation(ledgeForward, Vector3.up);    
+        stateMachine.animator.CrossFadeInFixedTime(HangHash, CrossFadeDuration);
+
+        stateMachine.ledgeDetector.OnLedgeDetect += HandleLedgeDetect;
+
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        if(stateMachine.InputReader.MovementValue.y < 0f)
+        {
+            stateMachine.SwitchState(new PlayerFallState(stateMachine));
+        }
+            
+    }
+
+    public override void Exit()
+    {
+        stateMachine.ledgeDetector.OnLedgeDetect -= HandleLedgeDetect;
+    }
+
+    private void HandleLedgeDetect(Vector3 closestPoint, Vector3 ledgeForward)
+    {
+
+    }
+
+}
