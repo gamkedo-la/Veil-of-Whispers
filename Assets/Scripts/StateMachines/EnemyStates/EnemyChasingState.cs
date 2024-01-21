7e071286d6d82406c58b515f8a75f6cb7cfe89f8
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2024-01-21 20:35:36 +0500

    Added Cube

--- Assets/Scripts/StateMachines/EnemyStates/EnemyChasingState.cs
+++ Assets/Scripts/StateMachines/EnemyStates/EnemyChasingState.cs
@@ -39,6 +39,14 @@ public class EnemyChasingState : EnemyBaseState
         audioState.PlayFootStepSound();
 
         MoveToPlayer(deltaTime);
+
+        RaycastHit raycast;
+        if (Physics.Raycast(stateMachine.transform.position,Vector3.down,out raycast))
+        {
+            Debug.Log((stateMachine.transform.position - raycast.point).y);
+          //  stateMachine.transform.position = raycast.point + Vector3.up * 10f;
+        }
+
         FacePlayer();
         stateMachine.animator.SetFloat(MoveHash, 1f, AnimatorDampTime, deltaTime);
 
@@ -56,10 +64,19 @@ public class EnemyChasingState : EnemyBaseState
 
         stateMachine.Agent.destination = stateMachine.Player.transform.position;
         Vector3 flatDesired = stateMachine.Agent.desiredVelocity.normalized;
-        flatDesired.y = 0f;
+       /*if(flatDesired.y > 0f)
+        {
+            flatDesired.y = 0f;
+       }*/
+
         Move(flatDesired.normalized * stateMachine.MovementSpeed, deltaTime);
         Vector3 flatVelocity = stateMachine.Controller.velocity;
-        flatVelocity.y = 0f;
+
+      /*  if (flatVelocity.y > 0f)
+        {
+            flatVelocity.y = 0f;
+        }*/
+
 
         stateMachine.Agent.velocity = flatVelocity;
     }
