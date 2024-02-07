eb941c79b2aa1e4fff5eb2a66bcf4d9ff36e831b
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2024-02-08 02:41:51 +0500

    Added intro Handler

---
+++ Assets/Scripts/IntroHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroHandler : MonoBehaviour
+{
+    public GameObject handleIntro;
+    public AudioClip introClip;
+    float timer;
+    bool sound;
+
+    private void Start()
+    {
+        handleIntro.SetActive(false);
+        sound = false;
+    }
+
+    void Update()
+    {
+        timer = Time.time;
+
+        if(timer > 0.2)
+        {
+            handleIntro.SetActive(true);
+
+            if(!sound)
+            {
+                AudioSource.PlayClipAtPoint(introClip, Camera.main.transform.position);
+                sound = true;
+            }
+
+            timer = 0;
+        }
+
+
+        if (timer > 1.5f)
+        {
+            handleIntro.SetActive(false);
+        }
+    }
+}
