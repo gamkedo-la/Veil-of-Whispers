37f8099e153ab22aed8ef3222233efcd8cb77dbd
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2024-02-07 20:41:03 +0500

    Resolved Menu scene issue

---
+++ Assets/Scripts/PrisonerCompass.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PrisonerCompass : MonoBehaviour
+{
+    public Transform player;
+    public Transform prisoner;
+    public TMP_Text displayLocation;
+
+  
+    void Update()
+    {
+        int dist = (int)Vector3.Distance(player.transform.position, prisoner.transform.position);
+        displayLocation.text = "PRISONER Compass: " + dist.ToString();
+    }
+}
