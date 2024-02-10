3247202e1ea14e0d2c26069a83ef855abd11d9cd
Chris DeLeon <gamkedo@gmail.com>
2024-02-09 23:45:33 -0500

    Credits integrated, quick change to game UI/story to scale with screen size

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,16 +8,6 @@ namespace UI
     {
         [SerializeField]
         private Button PlayButton;
-        [SerializeField]
-        private Button SettingsButton;
-        [SerializeField]
-        private Button MenuButton;
-        [SerializeField]
-        private Button options;
-        [SerializeField]
-        private Button back;
-        public GameObject optionsMenu;
-        public GameObject compass;
 
         private void Start()
         {
@@ -25,32 +15,7 @@ namespace UI
             {
                 SceneManager.LoadScene("Level");
             });
-            SettingsButton.onClick.AddListener(() =>
-            {
-                Debug.Log("Settings button");
-            });
-            MenuButton.onClick.AddListener(() =>
-            {
-               SceneManager.LoadScene("MainMenu");
-            });
 
-
-
-            options.onClick.AddListener(() =>
-            {
-                optionsMenu.SetActive(true);
-                compass.SetActive(false);
-                Time.timeScale = 0;
-            });
-
-
-            back.onClick.AddListener(() =>
-            {
-               optionsMenu.SetActive(false);
-               compass.SetActive(true);
-
-                Time.timeScale = 1;
-            });
         }
     }
 }
